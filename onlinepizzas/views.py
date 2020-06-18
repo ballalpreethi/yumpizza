c6e0f8bd3e8c5ecdaa9afa9292b18f73a8ecb6d4
Preethi <ballalpreethi@gmail.com>
2020-06-18 02:42:42 +0200

    added usd display

--- onlinepizzas/views.py
+++ onlinepizzas/views.py
@@ -64,7 +64,8 @@ def confirm_order(request):
             ordered_quantities.append(int(qty))
             totals.append(item.price*int(qty))
     return render(request, 'order_confirmation.html', {"order_summary": zip(ordered_pizzas, ordered_quantities, totals),
-                                                       "grand_total": sum(totals)+5, "grand_total_usd": sum(totals)*1.12})
+                                                       "grand_total": sum(totals)+5,
+                                                       "grand_total_usd": round(float(sum(totals)+5)*1.12, 2)})
 
 
 def place_order(request):
